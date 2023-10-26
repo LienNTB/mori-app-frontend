0efc43eb9cc7b152511199a796787b1eda9fdcae
qhuong1008 <qhuong1008@gmail.com>
2023-10-26 13:31:47 +0700

    add pagination, danhmuc on header

---
+++ src/app/book-category/[slug]/page.jsx
@@ -0,0 +1,105 @@
+"use client"
+import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
+import React, { useEffect, useState } from 'react'
+import styles from "./book-category.module.scss"
+import BookItem from '@/components/BookItem/BookItem'
+import Tag from '@/components/Tag/Tag'
+import { useDispatch, useSelector } from 'react-redux'
+import { getBooks } from '../../redux/actions/book'
+import Loading from '@/components/Loading/Loading'
+import { Pagination } from "@nextui-org/react";
+
+const BookWithCategory = () => {
+  const dispatch = useDispatch()
+  const books = useSelector(state => state.books.books)
+  const isLoading = useSelector(state => state.books.loading)
+
+  const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
+
+
+  return (
+    <div className={styles.homePageContainer}>
+      <Header />
+      {
+        isLoading ? <Loading />
+          :
+          <div className={styles.homePageContent}>
+            <div className={styles.headContent} >
+              <h1>Sách hay, hot, sách online miễn phí</h1>
+              <h2><i>Thư Viện Sách Ebook Miễn Phí</i></h2>
+              <div class={styles.description}>
+                <p>Đọc Sách Hay được xây dựng nhằm chia sẻ sách ebook miễn phí cho những ai khó khăn, chưa có điều kiện mua sách. Hãy để sách trải đường trên hành trình đưa bạn đi đến sự khôn ngoan và hiểu biết hơn.</p>
+                <p>Bản quyền sách thuộc về Tác giả &amp; Nhà xuất bản. Đọc Sách Hay khuyến khích các bạn nếu có khả năng hãy mua sách để ủng hộ Tác giả và Nhà xuất bản.</p>
+              </div>
+            </div>
+            <section className={styles.bookSectionContainer}>
+              <div class={styles.sectionHeader}>
+                <h3>Thể loại sách</h3>
+              </div>
+              <div className={styles.ruler}>
+
+              </div>
+              <div className={styles.sectionBody}>
+                <div className={styles.tagList}>
+
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" /><Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" />
+                  <Tag name="Tâm lý" link="tamly" /><Tag name="Tâm lý" link="tamly" />
+                </div>
+              </div>
+            </section>
+            <section className={styles.bookSectionContainer}>
+              <div class={styles.sectionHeader}>
+                <h3>Sách hay nên đọc</h3>
+                <a title="Sách hay nên đọc" href="https://docsachhay.net/cat/tam-ly-ky-nang-song/xem-nhieu-nhat-thang" class="getmorebtn">Xem thêm</a>
+              </div>
+              <div className={styles.ruler}>
+
+              </div>
+              <div className={styles.sectionBody}>
+                <div className={styles.bookList}>
+                  {
+                    books.map(book => {
+                      return (
+                        <div className={styles.bookItem}>
+                          <BookItem book={book} />
+                        </div>
+                      )
+                    })
+                  }
+
+                </div>
+              </div>
+            </section>
+            <div className={styles.pagination} >
+              <Pagination total={10} initialPage={1}
+                page={currentPage}
+                onChange={setCurrentPage} />
+            </div>
+          </div>
+
+      }
+      <Footer />
+    </div>
+  )
+}
+
+
+export default BookWithCategory
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,11 @@ import { useRouter } from 'next/navigation'
 import { UserAuth } from '@/app/context/AuthContext'
 import Image from "next/image";
 import logo from '../../../public/logo-nobg.png'
-
+import { Listbox, ListboxItem } from "@nextui-org/react";
+import { useState } from 'react'
 
 const Header = () => {
+  const [isOpenListbox, setIsOpenListbox] = useState(false)
   const router = useRouter();
   const { user, logOut } = UserAuth();
 
@@ -21,6 +23,21 @@ const Header = () => {
     }
   }
 
+  const items = [
+    {
+      key: "tamli",
+      label: "Tâm lí - kỹ năng sống",
+    },
+    {
+      key: "triethoc",
+      label: "Triết học",
+    },
+    {
+      key: "vanhoc",
+      label: "Văn học",
+    },
+
+  ];
   return (
     <div className={styles.container}>
       <div className={styles.topMenu}>
@@ -51,14 +68,35 @@ const Header = () => {
       </div>
       <div className={styles.bottomMenu}>
         <ul>
-          <li>Danh mục</li>
-          <li>Sách đọc</li>
-          <li>Sách nói</li>
-          <li>Bảng xếp hạng</li>
+          <li className={styles.danhmuc} onClick={() => setIsOpenListbox(p => !p)}>Danh mục
+            {isOpenListbox ? <div className={`${styles.listbox} w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100`}>
+              <Listbox
+                items={items}
+                aria-label="Dynamic Actions"
+                onAction={(key) => alert(key)}
+                className={styles.listboxContent}
+              >
+                {
+                  items.map(item => (
+                    <ListboxItem
+                      key={item.key}
+                      color={"default"}
+                    >
+                      {item.label}
+                    </ListboxItem>
+                  ))
+                }
+              </Listbox>
+
+            </div> : <></>}
+          </li>
+          <li className={styles.bottomMenuItem}>Sách đọc</li>
+          <li className={styles.bottomMenuItem}>Sách nói</li>
+          <li className={styles.bottomMenuItem}>Bảng xếp hạng</li>
 
         </ul>
       </div>
-    </div>
+    </div >
   )
 }
 

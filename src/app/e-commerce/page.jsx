ded9dac74ccd70d718bfb297501004e699e62f56
qhuong1008 <qhuong1008@gmail.com>
2024-03-30 17:09:09 +0700

    update ui

---
+++ src/app/e-commerce/page.jsx
@@ -0,0 +1,138 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import styles from './eCommerce.module.scss'
+import Header from '@/components/Header/Header'
+import { useDispatch, useSelector } from 'react-redux';
+import { getBooks } from '../redux/actions/book';
+import CollectionSidebar from '@/components/CollectionSidebar/CollectionSidebar';
+import TopFilter from '@/components/TopFilter/TopFilter';
+import CollectionContent from '@/components/CollectionContent/CollectionContent';
+
+const eCommerce = () => {
+  const dispatch = useDispatch()
+  const [domLoaded, setDomLoaded] = useState(false);
+  const productList = useSelector(state => state.books.books)
+  const [currentProducts, setCurrentProducts] = useState([]);
+  const [isAscName, setIsAscName] = useState(false);
+  const [isDescName, setIsDescName] = useState(false);
+  const [isAscPrice, setIsAscPrice] = useState(false);
+  const [isDescPrice, setIsDescPrice] = useState(false);
+
+
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("cart") === null) {
+      var cart = [];
+      localStorage.setItem("cart", JSON.stringify(cart));
+      cart = JSON.parse(localStorage.getItem("cart"));
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }
+
+
+  const sortArray = (sortProperty, asc) => {
+    const sorted = [...currentProducts].sort((a, b) => {
+      if (sortProperty === "initialPrice") {
+        if (asc) {
+          return a[sortProperty] - b[sortProperty];
+        } else {
+          return b[sortProperty] - a[sortProperty];
+        }
+      } else {
+        if (asc) {
+          return a[sortProperty].localeCompare(b[sortProperty]);
+        } else {
+          return b[sortProperty].localeCompare(a[sortProperty]);
+        }
+      }
+    });
+    setCurrentProducts(sorted);
+  };
+
+  const handleChange = (filterName) => {
+    const name = filterName;
+    if (name === "ascName") {
+      if (isAscName) {
+        setIsAscName(false);
+      } else {
+        setIsDescName(false);
+        setIsAscPrice(false);
+        setIsDescPrice(false);
+        setIsAscName(true);
+        sortArray("name", true);
+      }
+    } else if (name === "descName") {
+      if (isDescName) {
+        setIsDescName(false);
+      } else {
+        setIsAscName(false);
+        setIsAscPrice(false);
+        setIsDescPrice(false);
+        setIsDescName(true);
+        sortArray("name", false);
+      }
+    } else if (name === "ascPrice") {
+      //if ascPrice is true, set to true
+      //esle check if descPrice is set to true,
+      //if yes, change to false then change ascPrice to true
+      if (isAscPrice) {
+        setIsAscPrice(false);
+      } else {
+        setIsAscName(false);
+        setIsDescName(false);
+        setIsDescPrice(false);
+        setIsAscPrice(true);
+        sortArray("initialPrice", true);
+      }
+    } else if (name === "descPrice") {
+      if (isDescPrice) {
+        setIsDescPrice(false);
+      } else {
+        setIsAscName(false);
+        setIsDescName(false);
+        setIsAscPrice(false);
+        setIsDescPrice(true);
+        sortArray("initialPrice", false);
+      }
+    }
+  };
+
+
+  const isCheckedFilter = (name) => {
+    if (name === "ascName") {
+      return isAscName;
+    } else if (name === "descName") {
+      return isDescName;
+    } else if (name === "ascPrice") {
+      return isAscPrice;
+    } else {
+      return isDescPrice;
+    }
+  };
+
+  useEffect(() => {
+    dispatch(getBooks())
+  }, [])
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+  return (
+    <div className={styles.eCommerceContainer}>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.collectionContainer}>
+          <CollectionSidebar
+            handleChange={handleChange}
+          />
+          <div className={styles.collectionContainer__right}>
+
+            <CollectionContent productList={productList} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default eCommerce
---
+++ src/components/TopFilter/TopFilter.jsx
@@ -0,0 +1,115 @@
+import styles from "./TopFilter.module.scss";
+import { Dropdown, Menu, ModalBody, ModalHeader, Spacer } from "@nextui-org/react";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Modal, Button, Text, Input, Row, Checkbox } from "@nextui-org/react";
+import { useState } from "react";
+
+function TopFilter({ brandList, isChecked, handleChange, isCheckedFilter }) {
+  const menuItems = [
+    { name: "Tên A → Z", filterName: "ascName" },
+    { name: "Tên Z → A", filterName: "descName" },
+    { name: "Giá giảm dần", filterName: "descPrice" },
+    { name: "Giá tăng dần", filterName: "ascPrice" },
+  ];
+
+  const [selected, setSelected] = useState("Sắp xếp");
+  const [visible, setVisible] = useState(false);
+  const handler = () => { setVisible(true); console.log("check") };
+  const closeHandler = () => {
+    setVisible(false);
+  };
+
+  const handleSelected = (e) => {
+    let filterType = menuItems.filter((item) => {
+      if (item.name === e.currentKey) {
+        return item.filterName;
+      }
+    });
+
+    handleChange(filterType[0].filterName.toString());
+  };
+
+  return (
+    <div className={styles.topFilterContainer}>
+
+      <div className={styles.topFilterLeft}>
+        <Dropdown>
+          <Button color="success" flat>
+            {selected}
+          </Button>
+          <Menu
+            aria-label="Single selection actions"
+            color="secondary"
+            items={menuItems}
+            disallowEmptySelection
+            selectionMode="single"
+            selectedKeys={selected.name}
+            onSelectionChange={handleSelected}
+          >
+            {(item) => (
+              <Item key={item.name} color="success">
+                {item.name}
+              </Item>
+            )}
+            hihi
+          </Menu>
+        </Dropdown>
+      </div>
+      <div className={styles.topFilterRight} onClick={handler}>
+        <FontAwesomeIcon icon={faFilter} /> Lọc
+      </div>
+      <Modal
+        scroll
+        width="700px"
+        closeButton
+        aria-labelledby="modal-title"
+        open={visible}
+        onClose={closeHandler}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={20} h1 weight="bold">
+            Tìm theo:
+          </Text>
+        </Modal.Header>
+        <Modal.Body h2 weight="bold" size={15}>
+          {/* <div>
+            <Text color="green" weight="bold" size={15}>
+              THỂ LOẠI            </Text>
+            {brandList.map((brand, index) => {
+              return (
+                <>
+                  <Checkbox
+                    color="success"
+                    defaultSelected={isChecked(brand)}
+                    name="brand"
+                    value={brand}
+                    onChange={() => handleChange(brand)}
+                  >
+                    {brand}
+                  </Checkbox>
+                  <Spacer />
+                </>
+              );
+            })}
+          </div> */}
+          hihi
+          <div>
+            <Text color="green" weight="bold" size={15}>
+              LOẠI SẢN PHẨM
+            </Text>
+            <Checkbox size="sm" color="success">
+              Đồ gia dụng
+            </Checkbox>
+            <Spacer />
+            <Checkbox size="sm" color="success">
+              Thời trang xanh
+            </Checkbox>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default TopFilter;

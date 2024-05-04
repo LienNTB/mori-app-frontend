8d30a17efe3f190419a19f53500a1b2d8f8c6425
qhuong1008 <qhuong1008@gmail.com>
2024-05-04 17:46:20 +0700

    Update timeUtils.js

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -5,16 +5,31 @@ export function getTimeElapsed(createdAtString) {
 
   const elapsedMillis = now - createdAtDate;
   const elapsedSeconds = Math.floor(elapsedMillis / 1000);
+  const elapsedMinutes = Math.floor(elapsedSeconds / 60);
+  const elapsedHours = Math.floor(elapsedMinutes / 60);
+  const elapsedDays = Math.floor(elapsedHours / 24);
+  const elapsedMonths = Math.floor(elapsedDays / 30);
+  const elapsedYears = Math.floor(elapsedMonths / 12);
 
   if (elapsedSeconds < 60) {
     return `${elapsedSeconds} giây trước`;
   }
 
-  const elapsedMinutes = Math.floor(elapsedSeconds / 60);
   if (elapsedMinutes < 60) {
     return `${elapsedMinutes} phút trước`;
   }
 
-  const elapsedHours = Math.floor(elapsedMinutes / 60);
-  return `${elapsedHours} giờ trước`;
+  if (elapsedHours < 24) {
+    return `${elapsedHours} giờ trước`;
+  }
+
+  if (elapsedDays < 30) {
+    return `${elapsedDays} ngày trước`;
+  }
+
+  if (elapsedMonths < 12) {
+    return `${elapsedMonths} tháng trước`;
+  }
+
+  return `${elapsedYears} năm trước`;
 }

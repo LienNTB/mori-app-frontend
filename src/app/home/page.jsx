b5ffc80661e53bae94055f8cd72b59650a706f1f
qhuong1008 <qhuong1008@gmail.com>
2024-06-03 23:06:04 +0700

    fix build  fail

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
 import styles from './home.module.scss'
-import Header from '@/components/Header/Header'
-import Hero from '@/components/Hero/Hero'
-import Features from '@/components/Features/Features'
-import Testimonials from '@/components/Testimonials/Testimonials'
-import Newsletter from '@/components/Newsletter/Newsletter'
+import Features from '../../components/Features/Features'
+import Testimonials from '../../components/Testimonials/Testimonials'
+import Newsletter from '../../components/Newsletter/Newsletter'
 
-const LandingPage = () => {
+export default function LandingPage() {
   return (
     <div className={styles.landingPageContainer}>
       <div className={styles.landingPageContent}>
-        <Hero />
+        {/* <Hero /> */}
         <Features />
         <Testimonials />
         <Newsletter />
@@ -19,4 +16,3 @@ const LandingPage = () => {
   )
 }
 
-export default LandingPage

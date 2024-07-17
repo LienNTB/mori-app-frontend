c4e079e04f5518843d97f233d67d90c349a01e8f
Bích Liên <94447923+LienNTB@users.noreply.github.com>
2024-07-17 22:22:59 +0700

    revert code

--- src/components/ChapterAudioPlayer/ChapterAudioPlayer.jsx
+++ src/components/ChapterAudioPlayer/ChapterAudioPlayer.jsx
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect } from "react";
 import styles from "./ChapterAudioPlayer.module.scss";
 import Hls from "hls.js";
 import * as types from "@/app/redux/types";
-
 const ChapterAudioPlayer = ({ chapter, book }) => {
   const audioRef = useRef(null);
   const hlsRef = useRef(null);
@@ -14,32 +13,19 @@ const ChapterAudioPlayer = ({ chapter, book }) => {
     const encodedChapter = encodeURIComponent(chapter.name);
     const encodedChapterAudio = encodeURIComponent(chapter.audio);
 
-    const audioSrc = `${types.BACKEND_URL}/api/bookaudio/${encodedBook}/${encodedChapter}/${encodedChapterAudio}`;
-
-    // Determine the file type based on the file extension
-    const isM3U8 = encodedChapterAudio.toLowerCase().endsWith(".m3u8");
-    const isMP3 = encodedChapterAudio.toLowerCase().endsWith(".mp3");
-
-    if (isM3U8 && Hls.isSupported()) {
+    // Kiểm tra xem trình duyệt có hỗ trợ HLS không
+    if (Hls.isSupported()) {
       hlsRef.current = new Hls();
-      hlsRef.current.loadSource(audioSrc);
+      hlsRef.current.loadSource(
+        `${types.BACKEND_URL}/api/bookaudio/${encodedBook}/${encodedChapter}/${encodedChapterAudio}`
+      );
       hlsRef.current.attachMedia(audio);
-    } else if (isMP3) {
-      audio.src = `${types.BACKEND_URL}/api/bookaudio/${encodedChapterAudio}`;
-      audio.load();
     }
 
-    // Auto-play the audio when loaded
+    // Bắt đầu với trạng thái là true khi component được gọi
     audio.addEventListener("loadeddata", () => {
       audio.play();
     });
-
-    // Clean up Hls.js instance on unmount
-    return () => {
-      if (hlsRef.current) {
-        hlsRef.current.destroy();
-      }
-    };
   }, [encodedChapterAudio]);
 
   return (
@@ -59,12 +45,10 @@ const ChapterAudioPlayer = ({ chapter, book }) => {
       <div className={styles.audioPayerContainer}>
         <div className={styles.audioPlayer}>
           <audio ref={audioRef} controls preload="auto">
-            {encodedChapterAudio.toLowerCase().endsWith(".mp3") && (
-              <source
-                src={`${types.BACKEND_URL}/api/bookaudio/${encodedChapterAudio}`}
-                type="audio/mpeg"
-              />
-            )}
+            <source
+              src={`${types.BACKEND_URL}/api/bookaudio/${encodedChapterAudio}`}
+              type="application/x-mpegURL"
+            />
           </audio>
         </div>
       </div>
